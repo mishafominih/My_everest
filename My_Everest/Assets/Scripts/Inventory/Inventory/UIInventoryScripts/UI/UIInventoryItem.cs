730a398cfba69eb638906402f3a54b8ceaf09112
tako24 <frenk6795@gmail.com>
2022-04-10 21:46:37 +0500

    Описание в инвентаре

--- My_Everest/Assets/Scripts/Inventory/Inventory/UIInventoryScripts/UI/UIInventoryItem.cs
+++ My_Everest/Assets/Scripts/Inventory/Inventory/UIInventoryScripts/UI/UIInventoryItem.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UIInventoryItem : UIItem
+public class UIInventoryItem : UIItem, IPointerEnterHandler,IPointerExitHandler
 {
     [SerializeField] private Image imageIcon;
     [SerializeField] private TextMeshProUGUI textAmount;
+    [SerializeField] private TextMeshProUGUI discription;
 
     public IInventoryItem item { get; private set; }
 
@@ -20,18 +22,32 @@ public class UIInventoryItem : UIItem
         }
 
         item = slot.Item;
-        imageIcon.sprite = item.Info.SpriteIcon;
-        
+        imageIcon.sprite = item.Info.SpriteIcon; 
         imageIcon.gameObject.SetActive(true);
+        
         var textAmountEnabled = slot.Amount > 1;
         textAmount.gameObject.SetActive(textAmountEnabled);
         if (textAmountEnabled)
             textAmount.text = $"x{slot.Amount}";
+
+        discription.text = item.Info.Description;
+        discription.gameObject.SetActive(false);
     }
 
     private void CleanUp()
     {
         textAmount.gameObject.SetActive(false);
         imageIcon.gameObject.SetActive(false);
+        discription.gameObject.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        discription.gameObject.SetActive(!string.IsNullOrEmpty(discription.text));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        discription.gameObject.SetActive(false);
     }
 }
